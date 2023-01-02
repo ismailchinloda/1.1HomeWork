ec0217d90a15b459244cc04b0a789e4c43acd1b6
ismailchinloda <113357847+ismailchinloda@users.noreply.github.com>
2023-01-02 19:40:42 +0600

    Add files via upload

---
+++ 1.1 homeWork/1.1 homeWork/main.swift
@@ -0,0 +1,87 @@
+//
+//  main.swift
+//  1.1 homeWork
+//
+//  Created by Ismail Chinloda on 1/1/23.
+//
+
+import Foundation
+
+
+
+//№1 Создать 20 переменных (10 “let” и 10 “var”) с разными типами данных и их вариантами и вывести их в консоль
+//let eg1:String = "privet"
+//let eg2:Bool = true
+//let eg3:Double = 1.3
+//let eg4:Character = "f"
+//let eg5:Int = 4365
+//let eg6:String = "kak dela"
+//let eg7:Bool = false
+//let eg8:Double = 65.7
+//let eg9:Character = "g"
+//let eg10:Int = 3425432
+//
+//var egvar1:String = "ggggg"
+//var egvar2:Bool = true
+//var egvar3:Double = 980.3
+//var egvar4:Character = "g"
+//var egvar5:Int = 088
+//var egvar6:String = "ggggg"
+//var egvar7:Bool = false
+//var egvar8:Double = 9.7
+//var egvar9:Character = "p"
+//var egvar10:Int = 3213
+//
+//print(eg1,eg2,eg3,eg4,eg5,eg6,eg7,eg8,eg9,eg10)
+//print(          )
+//print(egvar1,egvar2,egvar3,egvar4,egvar5,egvar6,egvar7,egvar8,egvar9,egvar10)
+
+
+
+
+
+
+//№2 Создать функцию, которая будет принимать число Int и выдавать в консоль таблицу умножения его от 1 до 10
+//func multiplication(int: Int) -> Int {
+//    return int * number
+//}
+//
+//let number: Int = 5
+//
+//var numbervar: Int = multiplication(int: number)
+//
+//print(multiplication(int: 7))
+
+
+
+//Доп:
+//Создать “Анкету”
+//Параметры :
+//Имя
+//Фамилия
+//Возраст
+//Город проживания
+//Телефон
+//
+//В конце после ввода данных выдавать в консоль: (например)
+//“Имя - Айбек
+//Фамилия - Сарыбашев
+//Возраст - 26
+//Город проживания - Балыкчы
+//Телефон - 0999999999”
+//func anketa(name: String,surname: String,age: String,accommodation: String,number: String){
+//    print("Имя - \(name1)")
+//    print("фамилия - \(surname1)")
+//    print("возраст - \(age1)")
+//    print("проживание - \(accommodation1)")
+//    print("номер телефона - \(number1)")
+//}
+//
+//let name1 = readLine()!
+//let surname1 = readLine()!
+//let age1 = readLine()!
+//let accommodation1 = readLine()!
+//let number1 = readLine()!
+//
+//anketa(name: name1, surname: surname1, age: age1, accommodation: accommodation1, number: number1)
+
